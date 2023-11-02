1a7a898c3412a67ba1cdffd7d5b200ea588e8fe6
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-11-02 19:27:08 -0400

    Changed from bcrypt to bcryptjs

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema ({
     username: {
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema ({
 
 
 userSchema.pre("save", async function(){
-    this.password = await bcrypt.hash(this.password, 12);
+    this.password = await bcrypt.hashSync(this.password, 12);
 });
 
 module.exports = mongoose.model("User", userSchema);
